62260d823485d3787246d30e6a1bf8cd4ef287a5
Seiei Higa <hanachin@gmail.com>
2014-08-15 03:25:20 +0900

    Add yard document

--- lib/idobadge.rb
+++ lib/idobadge.rb
@@ -1,6 +1,29 @@
 require "idobadge/version"
 
+# Text badges generator for idobata.io generic webhook.
 module Idobadge
+  # @!method success(text, icon: 'sun-o')
+  #   @!scope class
+  #   Generate success badge html.
+  #   the default icon is {http://fontawesome.io/icon/sun-o/ sun-o}.
+  #   @param text [String] the badge content
+  #   @param icon [String, nil] the badge icon name You can use
+  #     {http://fontawesome.io/ Font Awesome} icon name or nil for empty icon
+  #   @return [String] badge html
+  # @!method warning(text, icon: 'umbrella')
+  #   @!scope class
+  #   Generate warning badge html.
+  #   the default icon is {http://fontawesome.io/icon/umbrella/ umbrella}.
+  #   @param text (see Idobadge.success)
+  #   @param icon (see Idobadge.success)
+  #   @return (see Idobadge.success)
+  # @!method important(text, icon: 'bomb')
+  #   @!scope class
+  #   Generate important badge html.
+  #   the default icon is {http://fontawesome.io/icon/bomb/ bomb}.
+  #   @param text (see Idobadge.success)
+  #   @param icon (see Idobadge.success)
+  #   @return (see Idobadge.success)
   {success: 'sun-o', warning: 'umbrella', important: 'bomb'}.each do |m, i|
     define_method(m) do |text, icon: i|
       "<span class=\"label label-#{__method__}\">" \
@@ -11,6 +34,10 @@ module Idobadge
     module_function m
   end
 
+  # Alias for .important
+  # @param text (see .success)
+  # @param icon (see .success)
+  # @return (see .success)
   alias :failure :important
   module_function :failure
 end
--- lib/idobadge/version.rb
+++ lib/idobadge/version.rb
@@ -1,3 +1,4 @@
 module Idobadge
+  # Current gem version
   VERSION = "0.0.2"
 end
